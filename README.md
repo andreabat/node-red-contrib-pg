bb60331233f5fc84fa719f7a932a007ae57fff15
Andrea Batazzi <andrea.batazzi@gmail.com>
2020-05-02 17:00:39 +0200

    Added parametrized queries
    Solved multiple connection problems
    
    Updated dependencies

--- README.md
+++ README.md
@@ -1,4 +1,68 @@
-# node-red-contrib-pg
-Node Red node for postgresql
-Based on postgrestor, but adds features such as "named queries".
 
+
+# node-red-contrib-pg
+Node-red-contrib-postgrestor :space_invader: is a [**Node-RED**](http://nodered.org/) node allowing basic access to [**Postgres**](https://www.postgresql.org/) :elephant: database.
+
+This fork has updated dependencies and allows to **Parameterized query**
+
+
+Pass them as a parameter array  ***params** of the msg object.
+
+Node-red-contrib-postgrestor sets up a console to execute queries against the configured database.
+
+```msg.payload``` will contain the result object of the query. It has the following properties:
+* ```command```: The sql command that was executed (e.g. "SELECT", "UPDATE", etc.)
+* ```rowCount```: The number of rows affected by the SQL statement
+* ```oid```: The oid returned
+* ```rows```: An array of rows
+
+
+Postgres implements a template engine allowing parameterized queries:
+```sql
+/* INTEGER id COLUMN */
+SELECT * FROM table WHERE id = {{ msg.id }}
+
+/* VARCHAR id COLUMN */
+SELECT * FROM table WHERE id = '{{ msg.id }}'
+
+
+
+SELECT * FROM table where name = $1;
+
+```
+
+msg.params = ['Andrea'] 
+
+## Installation
+
+#### Using the Node-RED Editor
+From version 0.15 of [**Node-RED**](http://nodered.org/) you can install [**node-red-contrib-postgrestor**](https://github.com/ymedlop/node-red-contrib-postgrestor) directly using the editor. To do this select ```Manage Palette``` from the menu (top right), and then select the ```install``` tab in the palette.
+
+You can now search for [**node-red-contrib-postgrestor**](https://github.com/ymedlop/node-red-contrib-postgrestor) to install.
+
+
+#### Installing npm packaged nodes
+To install [**node-red-contrib-postgrestor**](https://github.com/ymedlop/node-red-contrib-postgrestor) npm-packaged node, you can also, either install it locally within your user data directory (by default, ```$HOME/.node-red```):
+```bash
+cd $HOME/.node-red
+npm i node-red-contrib-postgrestor
+```
+or globally alongside Node-RED:
+```bash
+npm i -g node-red-contrib-postgrestor
+```
+You will need to restart Node-RED for it to pick-up [**node-red-contrib-postgrestor**](https://github.com/ymedlop/node-red-contrib-postgrestor).
+
+
+[![NPM](https://nodei.co/npm/node-red-contrib-postgrestor.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/node-red-contrib-postgrestor/)
+
+
+## Screen shots
+<p align="center">
+<img src="http://i.imgur.com/D03T3vH.png" width="600">
+<img src="https://i.imgur.com/43qkUp5.png" width="600">
+<img src="https://i.imgur.com/lJjLJTN.png" width="600">
+<img src="https://i.imgur.com/yYaO09Q.png" width="600">
+<img src="https://i.imgur.com/fHBxpB9.png" width="600">
+<img src="https://i.imgur.com/7vDSn1r.png" width="600">
+</p>
---
+++ postgres.js
@@ -0,0 +1,117 @@
+module.exports = function(RED) {
+    'use strict';
+    const mustache = require('mustache');
+    const Pool = require('pg').Pool;
+  
+    function getField(node, kind, value) {
+      switch (kind) {
+        case 'flow': {
+          return node.context().flow.get(value);
+        }
+        case 'global': {
+          return node.context().global.get(value);
+        }
+        case 'num': {
+          return parseInt(value);
+        }
+        case 'bool': {
+          return JSON.parse(value);
+        }
+        default: {
+          return value;
+        }
+      }
+    }
+  
+    function PgDBNode(n) {
+      const node = this;
+      RED.nodes.createNode(node, n);
+      node.name = n.name;
+      node.host = n.host;
+      node.hostFieldType = n.hostFieldType;
+      node.port = n.port;
+      node.portFieldType = n.portFieldType;
+      node.database = n.database;
+      node.databaseFieldType = n.databaseFieldType;
+      node.ssl = n.ssl;
+      node.sslFieldType = n.sslFieldType;
+      node.max = n.max;
+      node.maxFieldType = n.maxFieldType;
+      node.min = n.min;
+      node.minFieldType = n.minFieldType;
+      node.idle = n.idle;
+      node.idleFieldType = n.idleFieldType;
+      node.user = n.user;
+      node.userFieldType = n.userFieldType;
+      node.password = n.password;
+      node.passwordFieldType = n.passwordFieldType;
+    }
+  
+    RED.nodes.registerType('pgDB', PgDBNode);
+  
+    let myPool = false;
+  
+    function PgNode(config) {
+      const node = this;
+      RED.nodes.createNode(node, config);
+      node.topic = config.topic;
+      node.config = RED.nodes.getNode(config.pgDB);
+      node.on('input', (msg) => {
+        const query = mustache.render(config.query, { msg });
+        const {
+          user,
+          password,
+          host,
+          port,
+          database,
+          ssl,
+          max,
+          min,
+          iddle,
+          userFieldType,
+          passwordFieldType,
+          hostFieldType,
+          portFieldType,
+          databaseFieldType,
+          sslFieldType,
+          maxFieldType,
+          minFieldType,
+          idleFieldType
+        } = node.config;
+        myPool =
+          myPool ||
+          new Pool({
+            user: getField(node, userFieldType, user),
+            password: getField(node, passwordFieldType, password),
+            host: getField(node, hostFieldType, host),
+            port: getField(node, portFieldType, port),
+            database: getField(node, databaseFieldType, database),
+            ssl: getField(node, sslFieldType, ssl),
+            max: getField(node, maxFieldType, max),
+            min: getField(node, minFieldType, min),
+            idleTimeoutMillis: getField(node, idleFieldType, iddle)
+          });
+        const asyncQuery = async () => {
+          let client = false;
+          try {
+            client = await myPool.connect();
+            msg.payload = await client.query(query,msg.params||[]);
+            
+          } catch (err) {
+            const error = err.toString();
+            node.error(error);
+            msg.payload = error;
+          } finally {
+            if (client) {
+              client.release();
+            }
+            node.send(msg);
+          }
+        };
+        asyncQuery();
+      });
+      node.on('close', () => node.status({}));
+    }
+  
+    RED.nodes.registerType('pg', PgNode);
+  };
